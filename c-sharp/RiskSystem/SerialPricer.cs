0fcb78336c820f1bbf23b9e99964c511c121223e
Shaadowmaster <Ike.milian-lewis@warwick.ac.uk>
2026-01-15 19:48:44 +0000

    Initial draft of SerialPricer.LoadPrices

--- c-sharp/RiskSystem/SerialPricer.cs
+++ c-sharp/RiskSystem/SerialPricer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Reflection;
 using HmxLabs.TechTest.Models;
 
 namespace HmxLabs.TechTest.RiskSystem
@@ -28,12 +27,32 @@ namespace HmxLabs.TechTest.RiskSystem
 
         private void LoadPricers()
         {
-            var pricingConfigLoader = new PricingConfigLoader { ConfigFile = @".\PricingConfig\PricingEngines.xml" };
+            var pricingConfigLoader = new PricingConfigLoader { ConfigFile = @"PricingConfig/PricingEngines.xml" };
             var pricerConfig = pricingConfigLoader.LoadConfig();
+            
+            // Dynamic loading these like this will work, but the requirement for no compile-time dependency is strange and I would want to know why it
+            // exists/if there is an alternative solution. This is very fragile.
+            string assemblyPath = @"../../../../Pricers/bin/Debug/net8.0/Pricers.dll";
+            // This may throw, if it does this is a fatal issue for the program so we do not catch
+            Assembly asm = Assembly.LoadFrom(assemblyPath);
+            
 
             foreach (var configItem in pricerConfig)
             {
-                throw new NotImplementedException();
+                try
+                {
+                    Type type = asm.GetType(configItem.TypeName, throwOnError: true);
+                    var pricer = (IPricingEngine)Activator.CreateInstance(type);
+                    if (configItem.TradeType != null && pricer != null) 
+                        _pricers.Add(configItem.TradeType, pricer);
+                    else
+                        Console.WriteLine($"Error creating pricer for {configItem.TradeType}");
+                }
+                catch (Exception e)
+                {
+                    // We would use a proper logger not Console in production
+                    Console.WriteLine($"Exception when creating pricer for {configItem.TradeType} {e.Message}");
+                }
             }
         }
 
